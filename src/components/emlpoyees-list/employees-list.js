4719e15ae01778440a80bc669cfd3e9ac0c7f724
azamat <iskanderovak98@gmail.com>
2022-10-18 10:47:35 +0300

    first commit

---
+++ src/components/emlpoyees-list/employees-list.js
@@ -0,0 +1,22 @@
+import EmlpoyeesListItem from "../employees-list-item/employees-list-item";
+import "./employees-list.css";
+
+const EmlpoyeesList = ({ data, onDelete, onToggleProp }) => {
+  const elements = data.map((item) => {
+    const { id, ...itemProps } = item;
+    return (
+      <EmlpoyeesListItem
+        key={id}
+        {...itemProps}
+        onDelete={() => onDelete(id)}
+        onToggleProp={(event) =>
+          onToggleProp(id, event.currentTarget.getAttribute("data-toggle"))
+        }
+      />
+    );
+  });
+
+  return <ul className="app-list list-group">{elements}</ul>;
+};
+
+export default EmlpoyeesList;
